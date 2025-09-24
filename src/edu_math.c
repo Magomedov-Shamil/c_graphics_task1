6709450db0bc3532303380ec45f98a4c41e55088
Magomedov-Shamil <shamil20168@yandex.ru>
2025-09-24 20:10:45 +0300

    Update edu_math.c

--- src/edu_math.c
+++ src/edu_math.c
@@ -16,71 +16,63 @@ int edu_add(int a, int b) {
 }
 
 int edu_sub(int a, int b) {
-    UNUSED(a);
-    UNUSED(b);
-    UNIMPLEMENTED;
-    return 0;
+    return a - b;
 }
 
 int edu_mult(int a, int b) {
-    UNUSED(a);
-    UNUSED(b);
-    UNIMPLEMENTED;
-    return 0;
+    return a * b;
 }
 
 int edu_div(int a, int b) {
-    UNUSED(a);
-    UNUSED(b);
-    UNIMPLEMENTED;
-    return 0;
+    return a / b;
 }
 
 int edu_mod(int a, int b) {
-    UNUSED(a);
-    UNUSED(b);
-    UNIMPLEMENTED;
-    return 0;
+    return a % b;
 }
 
 int edu_pow(int a, int b) {
-    UNUSED(a);
-    UNUSED(b);
-    UNIMPLEMENTED;
-    return 0;
+    int n = 1;
+    for (int i = 0; i < b; i++) {
+        n *= a;
+    }
+    return n;
 }
 
 int edu_abs(int a) {
-    UNUSED(a);
-    UNIMPLEMENTED;
+    return a >= 0 ? a : -a;
 }
 
 int edu_gcd(int a, int b) {
-    UNUSED(a);
-    UNUSED(b);
-    UNIMPLEMENTED;
+    while (b != 0) {
+        int n = b;
+        b = a % b;
+        a = n;
+    }
+    return a;
 }
 
 int edu_lcm(int a, int b) {
-    UNUSED(a);
-    UNUSED(b);
-    UNIMPLEMENTED;
-    return 0;
+    return (a / edu_gcd(a, b)) * b;
 }
 
 int edu_int_sqrt(int a) {
-    UNUSED(a);
-    UNIMPLEMENTED;
-    return 0;
+    int n = 0;
+    while (n * n <= a) n++;
+    return n - 1;
 }
 
 int edu_sgn(int a) {
-    UNUSED(a);
-    UNIMPLEMENTED;
+    if (a == 0) return 0;
+    else if (a > 0) return 1;
+    else return -1;
 }
 
 int edu_fact(int a) {
-    UNUSED(a);
-    UNIMPLEMENTED;
-    return 0;
+    if (a == 0) return 1;
+	int n = 1;
+	for (int i = 1; i <= a; ++i) {
+		n *= i;
+	}
+	return n;
 }
